7d1dd03c8ccb769a654d22ad7e7dc96a7c804dd4
Lschanhi <lschanhi@outlook.com>
2025-08-18 21:17:01 -0300

    Aula 02 Projeto

---
+++ Notes/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+namespace Notes;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private void EnviarBtn_Clicked(object sender, EventArgs e)
+    {
+		Launcher.Default.OpenAsync("https://github.com/Lschanhi");
+    }
+
+  
+}
